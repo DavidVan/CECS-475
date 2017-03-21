9f6ac317deb2fe7caa2fdf6e844419abc8a6712d
David Van <David@DavidVan.net>
2017-03-20 22:41:29 -0700

    Fixed styling.

--- Lab 4/src/Cecs475.Othello.Application/OthelloView.xaml.cs
+++ Lab 4/src/Cecs475.Othello.Application/OthelloView.xaml.cs
@@ -15,68 +15,68 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Cecs475.Othello.Application {
-	/// <summary>
-	/// Interaction logic for OthelloView.xaml
-	/// </summary>
-	public partial class OthelloView : UserControl {
-		public static SolidColorBrush RED_BRUSH = new SolidColorBrush(Colors.Red);
-		public static SolidColorBrush GREEN_BRUSH = new SolidColorBrush(Colors.Green);
+   /// <summary>
+   /// Interaction logic for OthelloView.xaml
+   /// </summary>
+   public partial class OthelloView : UserControl {
+      public static SolidColorBrush RED_BRUSH = new SolidColorBrush(Colors.Red);
+      public static SolidColorBrush GREEN_BRUSH = new SolidColorBrush(Colors.Green);
 
-		public OthelloView() {
-			InitializeComponent();
-		}
+      public OthelloView() {
+         InitializeComponent();
+      }
 
-		private void Border_MouseEnter(object sender, MouseEventArgs e) {
-			Border b = sender as Border;
-			var square = b.DataContext as OthelloSquare;
-			var vm = FindResource("vm") as OthelloViewModel;
-			if (vm.PossibleMoves.Contains(square.Position)) {
-				b.Background = RED_BRUSH;
-			}
-		}
+      private void Border_MouseEnter(object sender, MouseEventArgs e) {
+         Border b = sender as Border;
+         var square = b.DataContext as OthelloSquare;
+         var vm = FindResource("vm") as OthelloViewModel;
+         if (vm.PossibleMoves.Contains(square.Position)) {
+            b.Background = RED_BRUSH;
+         }
+      }
 
-		private void Border_MouseLeave(object sender, MouseEventArgs e) {
-			Border b = sender as Border;
-			b.Background = GREEN_BRUSH;
-		}
+      private void Border_MouseLeave(object sender, MouseEventArgs e) {
+         Border b = sender as Border;
+         b.Background = GREEN_BRUSH;
+      }
 
-		public OthelloViewModel Model {
-			get { return FindResource("vm") as OthelloViewModel; }
-		}
+      public OthelloViewModel Model {
+         get { return FindResource("vm") as OthelloViewModel; }
+      }
 
-		private void Border_MouseUp(object sender, MouseButtonEventArgs e) {
-			Border b = sender as Border;
-			var square = b.DataContext as OthelloSquare;
-			var vm = FindResource("vm") as OthelloViewModel;
-			if (vm.PossibleMoves.Contains(square.Position)) {
-				vm.ApplyMove(square.Position);
-			}
-		}
-	}
+      private void Border_MouseUp(object sender, MouseButtonEventArgs e) {
+         Border b = sender as Border;
+         var square = b.DataContext as OthelloSquare;
+         var vm = FindResource("vm") as OthelloViewModel;
+         if (vm.PossibleMoves.Contains(square.Position)) {
+            vm.ApplyMove(square.Position);
+         }
+      }
+   }
 
-	/// <summary>
-	/// Converts from an integer player number to an Ellipse representing that player's token.
-	/// </summary>
-	public class OthelloSquarePlayerConverter : IValueConverter {
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-			int player = (int)value;
-			if (player == 0) {
-				return null;
-			}
+   /// <summary>
+   /// Converts from an integer player number to an Ellipse representing that player's token.
+   /// </summary>
+   public class OthelloSquarePlayerConverter : IValueConverter {
+      public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+         int player = (int) value;
+         if (player == 0) {
+            return null;
+         }
 
-			Ellipse token = new Ellipse() {
-				Fill = GetFillBrush(player)
-			};
-			return token;
-		}
+         Ellipse token = new Ellipse() {
+            Fill = GetFillBrush(player)
+         };
+         return token;
+      }
 
-		private static LinearGradientBrush GetFillBrush(int player) {
+      private static LinearGradientBrush GetFillBrush(int player) {
          LinearGradientBrush gradientBrush = new LinearGradientBrush();
          gradientBrush.StartPoint = new Point(0, 0);
          gradientBrush.EndPoint = new Point(1, 1);
 
 
-			if (player == 1) {
+         if (player == 1) {
             gradientBrush.GradientStops.Add(new GradientStop(Colors.Black, 0.5));
             gradientBrush.GradientStops.Add(new GradientStop(Colors.Red, 1.0));
          }
@@ -86,10 +86,10 @@ namespace Cecs475.Othello.Application {
          }
 
          return gradientBrush;
-		}
+      }
 
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-			throw new NotImplementedException();
-		}
-	}
+      public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+         throw new NotImplementedException();
+      }
+   }
 }
--- Lab 4/src/Cecs475.Othello.Application/OthelloViewModel.cs
+++ Lab 4/src/Cecs475.Othello.Application/OthelloViewModel.cs
@@ -5,87 +5,87 @@ using System.ComponentModel;
 using System.Collections.ObjectModel;
 
 namespace Cecs475.Othello.Application {
-	public class OthelloSquare : INotifyPropertyChanged {
-		private int mPlayer;
-		public int Player {
-			get { return mPlayer; }
-			set {
-				if (value != mPlayer) {
-					mPlayer = value;
-					OnPropertyChanged(nameof(Player));
-				}
-			}
-		}
+   public class OthelloSquare : INotifyPropertyChanged {
+      private int mPlayer;
+      public int Player {
+         get { return mPlayer; }
+         set {
+            if (value != mPlayer) {
+               mPlayer = value;
+               OnPropertyChanged(nameof(Player));
+            }
+         }
+      }
 
-		public BoardPosition Position {
-			get; set;
-		}
-		
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void OnPropertyChanged(string name) {
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-		}
-	}
+      public BoardPosition Position {
+         get; set;
+      }
 
-	public class OthelloViewModel : INotifyPropertyChanged {
-		private OthelloBoard mBoard;
-		private ObservableCollection<OthelloSquare> mSquares;
+      public event PropertyChangedEventHandler PropertyChanged;
+      private void OnPropertyChanged(string name) {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+      }
+   }
 
-		public event PropertyChangedEventHandler PropertyChanged;
-		private void OnPropertyChanged(string name) {
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-		}
+   public class OthelloViewModel : INotifyPropertyChanged {
+      private OthelloBoard mBoard;
+      private ObservableCollection<OthelloSquare> mSquares;
 
-		public OthelloViewModel() {
-			mBoard = new OthelloBoard();
-			mSquares = new ObservableCollection<OthelloSquare>(
-				from pos in (
-					from r in Enumerable.Range(0, 8)
-					from c in Enumerable.Range(0, 8)
-					select new BoardPosition(r, c)
-				)
-				select new OthelloSquare() {
-					Position = pos,
-					Player = mBoard.GetPieceAtPosition(pos)
-				}
-			);
+      public event PropertyChangedEventHandler PropertyChanged;
+      private void OnPropertyChanged(string name) {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+      }
 
-			PossibleMoves = new HashSet<BoardPosition>(mBoard.GetPossibleMoves().Select(m => m.Position));
-		}
+      public OthelloViewModel() {
+         mBoard = new OthelloBoard();
+         mSquares = new ObservableCollection<OthelloSquare>(
+            from pos in (
+               from r in Enumerable.Range(0, 8)
+               from c in Enumerable.Range(0, 8)
+               select new BoardPosition(r, c)
+            )
+            select new OthelloSquare() {
+               Position = pos,
+               Player = mBoard.GetPieceAtPosition(pos)
+            }
+         );
 
-		public void ApplyMove(BoardPosition position) {
-			var possMoves = mBoard.GetPossibleMoves() as IEnumerable<OthelloMove>;
-			foreach (var move in possMoves) {
-				if (move.Position.Equals(position)) {
-					mBoard.ApplyMove(move);
-					break;
-				}
-			}
+         PossibleMoves = new HashSet<BoardPosition>(mBoard.GetPossibleMoves().Select(m => m.Position));
+      }
 
-			PossibleMoves = new HashSet<BoardPosition>(mBoard.GetPossibleMoves().Select(m => m.Position));
-			var newSquares =
-				from r in Enumerable.Range(0, 8)
-				from c in Enumerable.Range(0, 8)
-				select new BoardPosition(r, c);
-			int i = 0;
-			foreach (var pos in newSquares) {
-				mSquares[i].Player = mBoard.GetPieceAtPosition(pos);
-				i++;
-			}
+      public void ApplyMove(BoardPosition position) {
+         var possMoves = mBoard.GetPossibleMoves() as IEnumerable<OthelloMove>;
+         foreach (var move in possMoves) {
+            if (move.Position.Equals(position)) {
+               mBoard.ApplyMove(move);
+               break;
+            }
+         }
+
+         PossibleMoves = new HashSet<BoardPosition>(mBoard.GetPossibleMoves().Select(m => m.Position));
+         var newSquares =
+            from r in Enumerable.Range(0, 8)
+            from c in Enumerable.Range(0, 8)
+            select new BoardPosition(r, c);
+         int i = 0;
+         foreach (var pos in newSquares) {
+            mSquares[i].Player = mBoard.GetPieceAtPosition(pos);
+            i++;
+         }
          OnPropertyChanged(nameof(BoardValue));
          OnPropertyChanged(nameof(CurrentPlayer));
          OnPropertyChanged(nameof(CanUndo));
       }
 
-		public ObservableCollection<OthelloSquare> Squares {
-			get { return mSquares; }
-		}
+      public ObservableCollection<OthelloSquare> Squares {
+         get { return mSquares; }
+      }
 
-		public HashSet<BoardPosition> PossibleMoves {
-			get; private set;
-		}
+      public HashSet<BoardPosition> PossibleMoves {
+         get; private set;
+      }
 
-		public int BoardValue { get { return mBoard.Value; } }
+      public int BoardValue { get { return mBoard.Value; } }
 
       public string CurrentPlayer {
          get {
@@ -122,5 +122,5 @@ namespace Cecs475.Othello.Application {
          OnPropertyChanged(nameof(CurrentPlayer));
          OnPropertyChanged(nameof(CanUndo));
       }
-	}
+   }
 }
