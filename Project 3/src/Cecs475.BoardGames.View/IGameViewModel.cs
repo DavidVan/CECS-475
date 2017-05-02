8d0c5d37c88b0346ce7a9f9a7718e8112b0448e5
David Van <David@DavidVan.net>
2017-05-02 11:23:18 -0700

    Restored comments.

--- Project 3/src/Cecs475.BoardGames.View/IGameViewModel.cs
+++ Project 3/src/Cecs475.BoardGames.View/IGameViewModel.cs
@@ -2,11 +2,33 @@
 using System.ComponentModel;
 
 namespace Cecs475.BoardGames.View {
-	public interface IGameViewModel : INotifyPropertyChanged {
-		int BoardValue { get; }
-		int CurrentPlayer { get; }
-		bool CanUndo { get; }
-		void UndoMove();
-		event EventHandler GameFinished;
-	}
+   /// <summary>
+   /// Represents a ViewModel for a game that can be played in a WPF application.
+   /// </summary>
+   public interface IGameViewModel : INotifyPropertyChanged {
+      /// <summary>
+      /// The value of the ViewModel's board.
+      /// </summary>
+      int BoardValue { get; }
+
+      /// <summary>
+      /// The current player of the ViewModel's board.
+      /// </summary>
+      int CurrentPlayer { get; }
+
+      /// <summary>
+      /// True if the board state can undo a move, false otherwise.
+      /// </summary>
+      bool CanUndo { get; }
+
+      /// <summary>
+      /// Undoes the most recent move applied to the game board.
+      /// </summary>
+      void UndoMove();
+
+      /// <summary>
+      /// Invoked when the game is finished.
+      /// </summary>
+      event EventHandler GameFinished;
+   }
 }
